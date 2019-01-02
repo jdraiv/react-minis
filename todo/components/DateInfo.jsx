0d34bb63d686df999f76672b4a02ea7112a3cade
Jdraiv <jdraiv@users.noreply.github.com>
2019-01-02 16:41:31 -0600

    Added DateInfo component

---
+++ todo/components/DateInfo.jsx
@@ -0,0 +1,39 @@
+
+export default class DateInfo extends React.Component {
+  // Props = [totalTasks]
+  constructor(props) {
+    super(props);
+    this.state = {month: "", year: "", date: ""}
+  }
+  
+  setDate() {
+    let date = new Date();
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec'];
+    
+    this.setState({
+      year: date.getFullYear(), 
+      month: months[date.getMonth()],
+      date: date.getDate()
+    })
+  }
+  
+  componentWillMount() {
+    this.setDate();
+  }
+  
+  render() {
+    return [
+      <div className="date-container">
+        <h4 id="date">{this.state.date}</h4>
+        <div className="sub-date-container">
+          <h4>{this.state.month}</h4>
+          <h4>{this.state.year}</h4>
+        </div>
+      </div>,
+      <div className="tasks-info-container">
+        <h4>{this.props.totalTasks} Task</h4>
+      </div>
+    ]
+  }
+}
