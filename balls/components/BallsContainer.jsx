6fd0a3d614a61f06fc77db99797f386f88351218
Jdraiv <jdraiv@users.noreply.github.com>
2019-01-16 11:24:56 -0600

    Create BallsContainer.jsx

---
+++ balls/components/BallsContainer.jsx
@@ -0,0 +1,35 @@
+
+export default class BallsContainer extends React.Component {
+  // Props = addBallEvent, choosenBalls, gameResult
+  constructor(props) {
+    super(props);
+    
+    this.state = {currentNum: 0}
+  }
+  
+  changeNumber() {
+    console.log(this.props.gameResult);
+    // FINISH THIS
+    if (this.state.currentNum != this.props.gameResult) {
+      //console.log("different")
+    }
+  }
+  
+  componentDidMount() {
+    this.interval = setInterval(() => this.changeNumber(), 1000)
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  
+  render() {
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const numbersList = numbers.map((num) => <Ball ballNumber={num} clickEvent={this.props.addBallEvent} choosenNumbers={this.props.choosenNumbers}/> )
+    return (                                 
+      <div className="balls-container">                           
+        {numbersList}
+      </div>                              
+    )
+  }
+}
